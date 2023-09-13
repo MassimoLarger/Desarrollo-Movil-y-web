1fea786bb80547fe5c0f59ca868c4db5aec0ab7f
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-09-13 16:56:45 -0300

    Actualizacion

---
+++ ACTIVIDADES/Js/03.variables.js
@@ -0,0 +1,14 @@
+var estatura = 1.75
+let peso = 55
+
+const nombre = "Massimo"
+
+var edad
+let ciudad
+edad = 21
+ciudad = "Osorno"
+
+const apellido = "Larger"
+
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
