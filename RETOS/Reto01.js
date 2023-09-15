ab5fe883b44981976fa3122a81c84b4aea2e05e7
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-09-15 12:41:13 -0300

    Actualizacion

--- RETOS/Reto01.js
+++ RETOS/Reto01.js
@@ -1,6 +1,8 @@
 let numero = prompt('¿Elige un número?',0);
 
-if ((numero % 2) = 0) {
+num = numero % 2;
+
+if (num == 0) {
     alert("El número elegido es par");
 } else {
     alert("El número elegido es impar");
