3240169d969f63158110e96b20c514309305736c
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-09-27 16:44:40 -0300

    Actualizacion

---
+++ Guia 1/1.js
@@ -0,0 +1,15 @@
+while (true)
+{
+let numero = prompt("Ingrese un número entero positivo:");
+
+if (isNaN(numero) || numero <= 0) {
+    alert("Por favor, ingrese un número entero positivo válido.");
+  } else {
+    let sumaPares = 0;
+
+    for (let i = 2; i <= numero; i += 2) {
+        sumaPares += i;
+      }
+      alert(`La suma de los numeros pares desde 1 hasta ${numero} es: ${sumaPares}`)
+    }
+}
