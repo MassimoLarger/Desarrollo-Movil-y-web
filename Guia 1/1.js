e483cb1aca38fffd4ad57f1d430481b97ab3c0ec
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-09-27 17:09:53 -0300

    Actualizacion

--- Guia 1/1.js
+++ Guia 1/1.js
@@ -10,6 +10,6 @@ if (isNaN(numero) || numero <= 0) {
     for (let i = 2; i <= numero; i += 2) {
         sumaPares += i;
       }
-      alert(`La suma de los numeros pares desde 1 hasta ${numero} es: ${sumaPares}`)
+      alert(`La suma de los numeros pares desde 1 hasta ${numero} es: ${sumaPares}`);
     }
 }
---
+++ Guia 1/2.js
@@ -0,0 +1,12 @@
+while (true)
+{
+let frase = prompt("Escriba una frase:");
+
+if (isFinite(frase)) {
+    alert("Por favor, ingrese un frase válida.");
+  } else {
+    let cortar = frase.split(" ");
+    let Contador = cortar.length;
+    alert(`Su frase es \"${frase}\" y tiene ${Contador} palabras`);
+    }
+}
