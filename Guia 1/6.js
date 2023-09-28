bef343adf227ec8a699ac2fa31ab2dfa5bca734d
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-09-27 23:52:45 -0300

    Actualizacion

--- Guia 1/6.js
+++ Guia 1/6.js
@@ -1,18 +1,26 @@
-while (true) {
-    let inventario = [];
+let inventario = [];
 
-    let nombre = prompt("Ingrese el nombre del objeto:");
+while (true) {
+    
+    let nombre = prompt("Ingrese el nombre del objeto (escriba fin para terminar):");
+
+    if (nombre.toLowerCase() === 'fin') {
+        break;
+    }
+
     let precio = prompt("Ingrese el precio del objeto:");
     let cantidad = prompt("Ingrese la cantidad a almacenar de este objeto:");
 
+    
+
     if (isNaN(precio) || isNaN(cantidad) || precio <= 0 || cantidad <= 0) {
-        alert("Por favor, ingrese valores válidos para precio y cantidad.");
+        alert("Por favor, ingrese valores válidos para precio y/o cantidad.");
     } else {
         inventario.push({ name: nombre, price: precio, stock: cantidad });
-        alert(inventario)
     }
+}
 
-    let stockCritico = false;
+let stockCritico = false;
 
     for (const producto of inventario) {
         if (producto.stock < 1000) {
@@ -24,4 +32,5 @@ while (true) {
     if (!stockCritico) {
         alert("El inventario está en buen estado. Todos los productos tienen un stock igual o superior a 1000 unidades.");
     }
-}
+
+    alert(inventario);
