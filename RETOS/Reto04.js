df32c923258615c0854b1dc46da187a1cb1d2c8f
MassimoLarger <massimoalberto.larger@alumnos.ulagos.cl>
2023-10-11 17:44:18 -0300

    Actualizacion

---
+++ RETOS/Reto04.js
@@ -0,0 +1,38 @@
+function calculadoraIMC() {
+    let nombre = document.getElementById('name').value;
+    let peso = parseInt(document.getElementById('weight').value);
+    let altura = parseInt(document.getElementById('height').value);
+    let activitadF = parseInt(document.getElementById('activity').value)
+  
+    if (isFinite(nombre) || isNaN(peso) || isNaN(altura)) {
+      alert('Por favor, ingrese números válidos.');
+    }
+    
+    let IMC = peso / ((altura/100)**2);
+    var Clasificacion = "";
+    let GEnergetico = (peso * activitadF);
+    var Estado = "";
+    
+    if (IMC < 18.5 ) {
+      var Clasificacion = "Bajo peso";
+    } else if (IMC > 18.5 && IMC < 24.9) {
+      var Clasificacion = "Peso normal";
+    } else if (IMC > 18.5 && IMC < 29.9) {
+      var Clasificacion = "Sobrepeso";
+    } else {
+      var Clasificacion = "Obesidad";
+    }
+
+    if (Clasificacion === "Bajo peso" || Clasificacion === "Sobrepeso" || Clasificacion === "Obesidad") {
+      var Estado = "Necesita Atención Especializada";
+    } else {
+      var Clasificacion = "Estado Nutricional Adecuado";
+    }
+
+    let resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = '<p><h1>Resultados para ' + nombre + ' </h1> </p>'+
+                              '<p>IMC: ' + IMC.toFixed(2) + ' </p>' +
+                              '<p>Clasificación: ' + Clasificacion + ' </p>' +
+                              '<p>Gasto Energético Diario: ' + GEnergetico.toFixed(2) + ' kcal </p>' +
+                              '<p>Estado Nutricional: ' + Estado + ' </p>';
+}
